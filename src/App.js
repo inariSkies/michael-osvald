148ee824ea57750b3d460a76d4155854fd9bb507
Michael Osvald <osvald.m@outlook.com>
2021-07-22 23:07:00 -0400

    test frontend values added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
+import MichaelHeadLogo from './img/MichaelHeadLogo.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={MichaelHeadLogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Oop okay looks like I'm alive now..
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+         I guess its time to re-Learn React
         </a>
       </header>
     </div>
