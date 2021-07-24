de5855859366d621a122d1cbafbf37023599c80c
Michael Osvald <osvald.m@outlook.com>
2021-07-24 00:41:44 -0400

    More formats considered

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import MichaelHeadLogo from './imgs/MichaelHeadLogo.gif';
 import './App.css';
+import sampleImg from './imgs/smpl.jpg'
 import theme from './theme';
-import { MuiThemeProvider, AppBar,Toolbar,IconButton, Card } from '@material-ui/core';
+import { MuiThemeProvider, AppBar,Toolbar,IconButton, Card, responsiveFontSizes } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { CardContent, Typography } from '@material-ui/core';
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-    <div className="App">
+
+
+<div className="App">
+{/*<img src={sampleImg} className="Back-Image"/>*/}
+<MuiThemeProvider theme={responsiveFontSizes(theme)}>    
       <AppBar  style={{backgroundColor:'#222', width:'98%', marginTop:'1%', marginRight:'1%'}}>
-        <Toolbar>
-        <Typography variant="h4" color="primary">
-          MICHAEL OSVALD
+        <Toolbar >
+        <Typography variant="h2" color="primary" style={{flex: '1'}}>
+          || michael osvald || 
         </Typography>
-        <div style={{flex: '1'}}></div>
+        
       
         <IconButton edge="end" color="inherit" aria-label="menu" style={{}} >
-  
         <MenuIcon> </MenuIcon> 
         </IconButton>
 
         </Toolbar>
       </AppBar>
-      <body>
-      <img src={MichaelHeadLogo} className="App-logo" alt="logo" />
-      <Card className = "Main-card" varient="outlined" style={{backgroundColor:'#222', width:'80%'}}>
+    <div style={{marginTop:'30%', width:'100%'}} >
+      <Card className = "Main-card" varient="outlined" style={{backgroundColor:'#222', width:'65%', marginBottom:'5%',marginRight:'30%'}}>
         <CardContent>
-          <Typography variant="h5" color="primary">
-          Oop okay looks like I'm alive now..
-         I guess its time to re-learn React
+         <Typography variant="h5" color="primary">
+          Hey is this website done yet?
           </Typography>
         </CardContent>
+      
       </Card>
-      </body>
-    </div>
+      <Card className = "Main-card2" varient="outlined" style={{ backgroundColor:'#222', width:'65%', marginLeft:'20%'}}>
+        <CardContent>
+         <Typography variant="h5" color="primary">
+         Hahahah uh no
+          </Typography>
+        </CardContent>
+
+      </Card>
+      </div>
+      <img src={MichaelHeadLogo} className="App-logo" alt="logo" />
+    
     </MuiThemeProvider>
+    </div>
+  
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,19 @@ import { createTheme } from "@material-ui/core";
 import { palette } from "@material-ui/system";
 export default createTheme({
     typeography:{
-        useNextVarietes:true
+        useNextVarietes:true,
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(','),
     },
     palette:{
         primary: {main: "#AAA", contrastText: "#FFF"},
