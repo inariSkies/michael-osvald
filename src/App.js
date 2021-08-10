38b018edac6b7d76b9eb5ba57de279244abebca9
Michael Osvald <osvald.m@outlook.com>
2021-08-09 22:47:43 -0400

    Route dom added, formatting for multipage(routes) began

--- src/App.js
+++ src/App.js
@@ -1,54 +1,86 @@
 import MichaelHeadLogo from './imgs/MichaelHeadLogo.gif';
 import './App.css';
-import sampleImg from './imgs/smpl.jpg'
-import ChatAnimation from './chatAnimation';
+import React, { useState, useReducer } from "react";
+import { Route, Link, Redirect } from "react-router-dom";
+import HomePage from './home';
 import theme from './theme';
-import { MuiThemeProvider, AppBar,Toolbar,IconButton, Grid, responsiveFontSizes,Switch } from '@material-ui/core';
+import { MuiThemeProvider, AppBar,Toolbar,IconButton, Grid, responsiveFontSizes,Switch, Menu, MenuItem } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { CardContent, Typography } from '@material-ui/core';
 
-function App() {
-  return (
 
+function App() {
+
+const [anchorEl, setAnchorEl] = useState(null);
+const handleClose = () => {
+  setAnchorEl(null);
+};
+const handleClick = event => {
+  setAnchorEl(event.currentTarget);
+};
+
+return (
 
 <div className="App">
-
 {/*<img src={sampleImg} className="Back-Image"/>*/}
 <MuiThemeProvider theme={responsiveFontSizes(theme)}>    
-      {/*<AppBar  style={{backgroundColor:'#222', width:'98%', marginTop:'1%', marginBottom:'1%', marginRight:'1%', position:'relative'}}>
-        <Toolbar >*/}
-        
-        <Typography variant="h2" color="primary" style={{flex: '1', marginTop:'10px', textShadow:'0 0 7px #05D9E8, 2px 5px 5px #005678', fontSize:'calc(36px + 2vmin)',  fontFamily: `'Press Start 2P', cursive`}}>
-           michael osvald 
-        </Typography>
-        <Typography >
-        <div>
-        <Grid component="label" container alignItems="center" spacing={1}>
-          <Grid item>Off</Grid>
-          <Grid item>
-           <Switch  name="c" />
-           {/*checked={state.checkedC} onChange={handleChange}*/}
-          </Grid>
-          <Grid item>On</Grid>
+  <AppBar  style={{backgroundColor:'#222', width:'90%', marginTop:'1%', marginBottom:'1%', marginRight:'1%', position:'relative'}}>
+    <Toolbar >
+    <IconButton
+            onClick={handleClick}
+            color="#fff"
+            style={{ marginLeft: "auto", paddingRight: "1vh" }}
+          >
+            <MenuIcon></MenuIcon>
+          </IconButton>
+    <Menu
+      id="simple-menu"
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+    >
+      <MenuItem component={Link} to="/home" onClick={handleClose}>
+        Home
+      </MenuItem>
+      </Menu>
+      </Toolbar>
+  </AppBar>
+  <Typography variant="h2" color="primary" style={{flex: '1', marginTop:'10px', textShadow:'0 0 7px #05D9E8, 2px 5px 5px #005678', fontSize:'calc(36px + 2vmin)',  fontFamily: `'Press Start 2P', cursive`}}>
+    michael osvald 
+  </Typography>
+  <Typography >
+    <div>
+      <Grid component="label" container alignItems="center" spacing={1}>
+        <Grid item>Off</Grid>
+        <Grid item>
+          <Switch  name="c" />
+          {/*checked={state.checkedC} onChange={handleChange}*/}
         </Grid>
-        </div>
+        <Grid item>On</Grid>
+      </Grid>
+    </div>
 
-      </Typography>
-      
-       {/*  <IconButton edge="end" color="inherit" aria-label="menu" >
-        <MenuIcon> </MenuIcon> 
-        </IconButton>
-      >
-        </Toolbar>
-      </AppBar>
- */}
-   
-      <ChatAnimation/>
-    </MuiThemeProvider>
+  </Typography>
+
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path="/home" component={HomePage} />
+       
+
+ 
+  
+    {/*  <IconButton edge="end" color="inherit" aria-label="menu" >
+    <MenuIcon> </MenuIcon> 
+    </IconButton>
+    >
+    </Toolbar>
+    </AppBar>
+  */}
+  
+  </MuiThemeProvider>
    
    {// <img src={MichaelHeadLogo} className="App-logo" alt="logo" />
    }
-    
+   
     </div>
    
   );
---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import ChatAnimation from './chatAnimation';
+import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
+import theme from "./theme";
+import "./App.css";
+const HomePage = () => {
+  return (
+    <MuiThemeProvider theme={theme}>
+       <ChatAnimation/>
+    </MuiThemeProvider>
+  );
+};
+export default HomePage;
